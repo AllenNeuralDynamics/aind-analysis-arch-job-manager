e38b912aab724b3b3050ca21d39253b3e55ab52b
Han Hou <han.hou@alleninstitute.org>
2024-09-22 03:32:34 +0000

    Edited run_capsule.py

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -169,7 +169,7 @@ if __name__ == "__main__":
         if if_random_job_order:
             random.shuffle(job_dicts_to_assign)
         
-        job_dicts_to_assign = job_dicts_to_assign[:(args.max_jobs or 10)]
+        job_dicts_to_assign = job_dicts_to_assign[:int(args.max_jobs or "10")]
         assign_jobs(job_dicts_to_assign, n_workers=int(args.n_workers or "20"))
     else:
         logger.info(f"No pending jobs to assign. {'-'*20}")
