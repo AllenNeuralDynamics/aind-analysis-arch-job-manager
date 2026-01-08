30c0f2342987c4252a21223030f55b378adda0f3
houhan@gmail.com <han.hou@alleninstitute.org>
2026-01-08 00:24:36 +0000

    typo

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -126,7 +126,7 @@ def generate_all_jobs() -> list:
                     "QLearning_L2F1_softmax",  # Hattori2019
                     "WSLS",  # Win-Stay-Lose-Shift
                     "QLearning_L2F1_CKfull_softmax",  # The model with the most parameters
-                    "Qlearning_L1F1_CKfull_softmax",  # Bari2019 with CKfull; per discussion 
+                    "QLearning_L1F1_CKfull_softmax",  # Bari2019 with CKfull; per discussion 
                                                       # with Bowen and Xinxin @ 1/7/2026
                 ]
             ),
